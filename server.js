6d186209979f8d65ae02b459d920ee26cc48332e
damien <damienlopa.pro@gmail.com>
2017-01-28 10:50:17 +0100

    BASE OF PROJECT

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-var http = require('http');
+// Tout d'abbord on initialise notre application avec le framework Express
+// et la bibliothèque http integrée à node.
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var server = http.createServer(function(req, res) {
-    res.writeHead(200);
-    res.end('Salut tout le monde !');
+// On gère les requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
+app.use("/", express.static(__dirname + "/public"));
+
+// On lance le serveur en écoutant les connexions arrivant sur le port 3000
+http.listen(3000, function(){
+    console.log('Server is listening on *:3000');
 });
-server.listen(8080);
